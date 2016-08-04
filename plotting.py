93227baef46f12605594df8e85e994f35b2aef5e
Tom de Ruijter <deruijter.tom@gmail.com>
2016-08-04 14:34:22 +0200

    GPy as predictor

--- plotting.py
+++ plotting.py
@@ -5,14 +5,16 @@ def add_background_map(ax, res='110m', alpha=0.5):
     ax.add_feature(cfeature.NaturalEarthFeature(
             'physical', 'land', res,
             edgecolor='face',
-            facecolor=cfeature.COLORS['land'],
+            facecolor='white',
+            # facecolor=cfeature.COLORS['land'],
             zorder=-1,
             alpha=alpha
     ))
     ax.add_feature(cfeature.NaturalEarthFeature(
             'physical', 'ocean', res,
             edgecolor='face',
-            facecolor=cfeature.COLORS['water'],
+            facecolor='white',
+            # facecolor=cfeature.COLORS['water'],
             zorder=2,
             alpha=alpha
     ))
@@ -31,17 +33,17 @@ def add_background_map(ax, res='110m', alpha=0.5):
             zorder=-1,
             alpha=alpha
     ))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-            'physical', 'lakes', res,
-            edgecolor='face',
-            facecolor=cfeature.COLORS['water'],
-            zorder=-1,
-            alpha=alpha
-    ))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-            'physical', 'rivers_lake_centerlines', res,
-            edgecolor=cfeature.COLORS['water'],
-            facecolor='none',
-            zorder=-1,
-            alpha=alpha
-    ))
+    # ax.add_feature(cfeature.NaturalEarthFeature(
+    #         'physical', 'lakes', res,
+    #         edgecolor='face',
+    #         facecolor=cfeature.COLORS['water'],
+    #         zorder=-1,
+    #         alpha=alpha
+    # ))
+    # ax.add_feature(cfeature.NaturalEarthFeature(
+    #         'physical', 'rivers_lake_centerlines', res,
+    #         edgecolor=cfeature.COLORS['water'],
+    #         facecolor='none',
+    #         zorder=-1,
+    #         alpha=alpha
+    # ))
