254e77c1822ed1c1a5bb6bd44e41f0e0cf86e17e
Tom de Ruijter <deruijter.tom@gmail.com>
2016-08-05 15:45:05 +0200

    Multiple issues

--- fs_loader.py
+++ fs_loader.py
@@ -1,5 +1,6 @@
 import logging
 import itertools
+import numpy as np
 import pandas as pd
 import joblib as jl
 
@@ -20,10 +21,12 @@ class DataDescriptor(object):
         return hash((self.element, self.forecast_hour))
 
     def __eq__(self, other):
-        return (self.element, self.forecast_hour) == (other.element, other.forecast_hour)
+        return (self.element, self.forecast_hour) == \
+               (other.element, other.forecast_hour)
 
     def __str__(self):
-        return 'DataDescriptor(element: %s, forecast hour: %d)' % (self. element, self.forecast_hour)
+        return 'DataDescriptor(element: %s, forecast hour: %d)' % \
+               (self. element, self.forecast_hour)
 
 
 def load_station_data():
@@ -36,7 +39,8 @@ def get_file_path(file_identifier):
     # TODO TdR 13/07/16: fix hard-coded data path.
     element = file_identifier.element
     forecast_hour = file_identifier.forecast_hour
-    return 'data/mgforsys_europe/%s/fh%d_D11_B3_T2_M5.csv' % (element, forecast_hour)
+    return 'data/mgforsys_europe/%s/fh%d_D11_B3_T2_M5.csv' % \
+           (element, forecast_hour)
 
 
 @memory.cache()
@@ -66,9 +70,11 @@ def load_multiple_files(file_identifiers):
 def load_dataset_for_forecast_hour(forecast_hour, request):
     predictors = request['model_elements']
     files_to_load = get_file_identifiers(predictors, [forecast_hour])
-    logging.debug("Data for forecast hour %d: loading %d file(s).." % (forecast_hour, len(files_to_load)))
+    logging.debug("Data for forecast hour %d: loading %d file(s).." %
+                  (forecast_hour, len(files_to_load)))
     dataset_map = load_multiple_files(files_to_load)
-    logging.debug("Data for forecast hour %d: successfully loaded %d files." % (forecast_hour, len(dataset_map)))
+    logging.debug("Data for forecast hour %d: successfully loaded %d files." %
+                  (forecast_hour, len(dataset_map)))
 
     data = pd.DataFrame()
     for data_id in dataset_map:
@@ -79,7 +85,8 @@ def load_dataset_for_forecast_hour(forecast_hour, request):
             # Perform inner join on two dataframes, joining on common columns.
             before_dtypes = data.dtypes
             data = pd.merge(data, element_data, how='inner')
-            assert (data.dtypes[before_dtypes.index] == before_dtypes).all(), "Data types changed after merge."
+            assert np.all(data.dtypes[before_dtypes.index] == before_dtypes),\
+                "Data types changed after merge."
 
     return data
 
@@ -107,7 +114,10 @@ def get_observation_column(request):
 def construct_dataset_column_names(request):
     observation_column = get_observation_column(request)
     feature_cols = get_feature_columns(request)
-    dataset_column_names = ['station_id', 'latitude', 'longitude', 'elevation', 'forecast_hour', 'valid_date']
+    dataset_column_names = [
+        'station_id', 'latitude', 'longitude',
+        'elevation', 'forecast_hour', 'valid_date'
+    ]
     dataset_column_names += [observation_column] + feature_cols
     return dataset_column_names
 
@@ -124,13 +134,21 @@ def row_concat(dfs):
 
 def clean_data(data):
     # TODO TdR 17/07/16: impute data & outlier filtering
+    # Imputing should be done per forecast hour
     nr_rows_before = len(data)
     # Drop rows having at least one NaN value.
-    data = data.dropna(axis=0, how='any')
     data = data.dropna(axis=0, how='any')
     nr_rows_after = len(data)
     logging.debug("Dropped %d rows." % (nr_rows_after - nr_rows_before))
     return data
+
+
+def filter_locations(df, area):
+    top_lat, bot_lat, left_lon, right_lon = area
+    return df[
+        (df.latitude >= bot_lat) & (df.latitude <= top_lat) &
+        (df.longitude >= left_lon) & (df.longitude <= right_lon)
+    ]
 
 
 def load_dataset(request):
@@ -144,7 +162,6 @@ def load_dataset(request):
     for forecast_hour in request['forecast_hours']:
         logging.info("Loading files for forecast hour %d.." % forecast_hour)
         fh_data = load_dataset_for_forecast_hour(forecast_hour, request)
-        # TODO TdR 13/07/16: filter out stations located outside of request
 
         if forecast_data.empty:
             forecast_data = fh_data
@@ -152,7 +169,25 @@ def load_dataset(request):
             forecast_data = row_concat([forecast_data, fh_data])
 
     forecast_data = pd.merge(forecast_data, station_data, how='inner')
-    forecast_data = forecast_data[column_names]
+    forecast_data = filter_locations(forecast_data, request['predict_area'])
+    forecast_data = forecast_data.filter(items=column_names)
+    forecast_data.valid_date = \
+        pd.to_datetime(forecast_data.valid_date.astype(str), format='%Y%m%d%H')
     forecast_data = clean_data(forecast_data)
 
     return forecast_data
+
+
+if __name__ == "__main__":
+    test_request = {
+        'predictant': 'TT2m',
+        'model_elements': ['TT2m', 'FF10m'],
+        'forecast_hours': [18],
+        'model_name': 'ModelMix',
+        'valid_date': '20151201',
+        'predict_area': (60, 33, -12, 20),
+        'predict_resolution': 0.1
+    }
+
+    df = load_dataset(test_request)
+    print("Stations in set: %d" % (len(df.station_id.unique())))
